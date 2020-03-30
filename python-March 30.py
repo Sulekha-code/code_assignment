993171a8e661ac38e1fc1871297f505f893012c2
Sulekha-code <61930656+Sulekha-code@users.noreply.github.com>
2020-03-30 23:53:30 +0530

    data conversion assignment

---
+++ python-March 30.py
@@ -0,0 +1,101 @@
+#int to float
+a=16
+b=float(a)
+
+print("int to float")
+print(b)
+print(type(b))
+
+#int to string
+a=10
+b=str(a)
+
+print("int to string")
+print(b)
+print(type(b))
+
+#int to bool
+a=11
+b=bool(a)
+
+print("int to bool")
+print(b)
+print(type(b))
+
+#float to int
+a=16.0
+b=float(a)
+
+print(b)
+print(type(b))
+
+#float to string
+a=12.0
+b=str(a)
+
+print("float to string")
+print(b)
+print(type(b))
+
+#float to bool
+a=13.0
+b=bool(a)
+
+print("float to bool")
+print(b)
+print(type(b))
+
+'''
+#string to int
+a="sulekha"
+b=int(a)
+
+print("string to int")
+print(b)
+print(type(b))
+'''
+
+'''
+#string to float
+a="sulekha"
+b=float(a)
+
+print("string to float")
+print(b)
+print(type(b))
+'''
+
+#string to bool
+a="sulekha"
+b=bool(a)
+
+print("string to bool")
+print(b)
+print(type(b))
+
+
+#bool to int
+a=True
+b=int(a)
+
+print("bool to int")
+print(b)
+print(type(b))
+
+
+#bool to float
+a=False
+b=float(a)
+
+print("bool to float")
+print(b)
+print(type(b))
+
+
+#bool to string
+a=True
+b=str(a)
+
+print("bool to string")
+print(b)
+print(type(b))
