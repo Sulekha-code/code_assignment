cc0d02b4ecaca47518e02197ca38de6e8437a285
Sulekha-code <61930656+Sulekha-code@users.noreply.github.com>
2020-04-01 02:16:07 +0530

    Operators
    Operators

---
+++ python March 31st-Assignment.py
@@ -0,0 +1,103 @@
+'''
+a=7
+b=9
+c="10"
+#d=int(input("Please enter a number:"))
+
+#input("Please enter a number:")
+input("sulekha")
+
+#d=int("15.30")
+print(d)
+
+'''
+
+
+'''
+a=int(input('enter a number'))
+print(a)
+
+b=int(input('enter a number'))
+print(b)
+c=a+b
+print(c)
+'''
+
+'''
+a=int(input("enter a integer"))
+print(a)
+
+b=float(input("enter float value"))
+print(b)
+
+c=bool(input("enter boolean value"))
+print(c)
+
+d=input("enter string value")
+print(d)
+
+e=a+b
+print(e)
+'''
+
+'''
+#Area of cirle
+
+pi=3.14
+r=float(input("enter radius"))
+Area=pi*r*r
+print(Area))
+'''
+'''
+#print a2-b2
+
+a=int(input("enter number"))
+print(a)
+
+b=int(input("enter number"))
+print(b)
+
+c=(a*a)-(b*b)
+print(c)
+'''
+
+'''
+#a+b*a-b
+a=int(input("enter number"))
+print(a)
+
+b=int(input("enter number"))
+print(b)
+
+c=(a+b)*(a-b)
+print(c)
+'''
+'''
+##(a+b)/2
+a=int(input("enter number"))
+print(a)
+
+b=int(input("enter number"))
+print(b)
+
+c=(a+b)/2
+print(c)
+'''
+'''
+
+a=int(input("enter number"))
+print(a)
+
+b=int(input("enter number"))
+print(b)
+
+c=a*a+b*b+2*a*b
+print(c)
+'''
+#pir2h
+pi=3.14
+r=int(input("enter number"))
+h=int(input("enter number"))
+sum=pi*r*r*h
+print(int(sum))
+
