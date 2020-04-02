b6745580483398715a3f01b41f86c83b80cb8135
Sulekha-code <61930656+Sulekha-code@users.noreply.github.com>
2020-04-02 07:17:56 +0530

    Program using format
    Program using format

---
+++ python March 1st-Assignment-Tasks.py
@@ -0,0 +1,145 @@
+'''
+a=7
+b=9
+c="10"
+#d=int(input("Please enter a number:"))
+
+#input("Please enter a number:")
+input("sulekha")
+
+#d=int("15.30")
+print(d)
+
+'''
+
+
+'''
+a=int(input('enter a number'))
+print(a)
+
+b=int(input('enter a number'))
+print(b)
+c=a+b
+print(c)
+'''
+
+'''
+a=int(input("enter a integer"))
+print(a)
+
+b=float(input("enter float value"))
+print(b)
+
+c=bool(input("enter boolean value"))
+print(c)
+
+d=input("enter string value")
+print(d)
+
+e=a+b
+print(e)
+'''
+
+'''
+#Area of cirle
+
+pi=3.14
+r=float(input("enter radius"))
+Area=pi*r*r
+print(Area))
+'''
+'''
+#print a2-b2
+
+a=int(input("enter number"))
+print(a)
+
+b=int(input("enter number"))
+print(b)
+
+c=(a*a)-(b*b)
+print(c)
+'''
+
+'''
+#a+b*a-b
+a=int(input("enter number"))
+print(a)
+
+b=int(input("enter number"))
+print(b)
+
+c=(a+b)*(a-b)
+print(c)
+'''
+'''
+##(a+b)/2
+a=int(input("enter number"))
+print(a)
+
+b=int(input("enter number"))
+print(b)
+
+c=(a+b)/2
+print(c)
+'''
+'''
+
+a=int(input("enter number"))
+print(a)
+
+b=int(input("enter number"))
+print(b)
+
+c=a*a+b*b+2*a*b
+print(c)
+'''
+'''
+#pir2h
+pi=3.14
+r=int(input("enter number"))
+h=int(input("enter number"))
+sum=pi*r*r*h
+print(int(sum))
+'''
+'''
+
+#perimeter of rectangle
+l=float(input("enter number"))
+print(l)
+
+b=float(input("enter number"))
+print(b)
+
+perimeter=2*(l+b)
+print(int(perimeter))
+'''
+'''
+#volume of cone
+pi=3.14
+r=int(input("enter radius"))
+print(r)
+h=int(input("enter hright"))
+print(h)
+volume=1/3*pi*r*r*h
+print(volume)
+print("volume of cone with radius{0}and height{1} is {2}".format(r,h,volume))
+'''
+'''
+#print pir2 in print format
+pi=3.14
+r=float(input("enter radius"))
+print(r)
+area=int(pi*r*r)
+print(area)
+print("area of circle with radius{0} is {1}".format(r,area))
+'''
+
+#print 2pir in print format
+pi=3.14
+r=float(input("enter radius"))
+print(r)
+circ=int(2*pi*r)
+print(circ)
+print("circumference of circle with radius{0} is {1}".format(r,circ))
+
