8bf854b1028e9254f68423e3468fd69c09a0986f
Sulekha-code <61930656+Sulekha-code@users.noreply.github.com>
2020-04-03 01:17:54 +0530

    #indexing and slicing
    #indexing and slicing

---
+++ python 02 April-Assigment Task.py
@@ -0,0 +1,77 @@
+#Indexing and slicing
+#forward indexing starts with o
+# 0 1 2 3 4 5
+# p y t h o n
+#-6-5-4-3-2-1
+#reverse indexing starts with -1
+'''
+val="python"
+print(val)
+print(len(val))
+print(type(val))
+print(val[0])
+print(val[-1])
+print(val[-2])
+'''
+#string is collection of characters
+#0  1  2   3  4  5  6  7
+#c  o  m   p  u  t  e  r
+#-8-7 -6  -5  -4 -3 -2 -1
+
+#val="computer"
+#print(val[-1])
+
+'''
+val="computer"
+print(val[7])
+'''
+'''
+val="computer"
+print(val[0])
+'''
+'''
+val="computer"
+print(val[-8])
+'''
+'''
+string="computer"
+#print(len(string)/2)
+#print(int(len(0string)/2))
+#print(string[int(len(string)/2)])
+mid = int(len(string)/2)
+print(mid)
+'''
+'''
+#mid value in enginnering mathematics
+value="Engineering_mathematics"
+print(len(value)/2)
+print(int(len(value)/2))
+mid=int(len(value)/2)
+print(value[mid])
+'''
+'''
+value="engineering_mathematics"
+print(value[-11:])
+print(value[2:10])
+print(value[15:])
+print(value[3:12])
+print(value[-19:9])
+print(value[2:9])
+print(value[-18:18])
+print(value[-18:])
+print(value[0:6])
+
+'''
+value="engineering_mathematics"
+
+print(value[-11:])
+
+print(value[:-12])
+
+print(value[-6:])
+
+print(value[-19:-15])
+
+print(value[-11:-8])
+
+
