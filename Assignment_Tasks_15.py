13c013d9853677c3816845fcfedaf11d0cf153fc
Sulekha-code <61930656+Sulekha-code@users.noreply.github.com>
2020-04-15 18:32:36 +0530

    Assigment task_15_April
    Assigment task_15_April

---
+++ Assignment_Tasks_15.py
@@ -0,0 +1,25 @@
+'''#Assignment taks
+##print only even numbers from <user input> to <user input> while
+n=int(input("enter number:")
+i=0
+while(i<n):
+    i=i+2
+    print(i)
+
+
+##multiples of 5 between 100 to 200
+n=200
+i=100
+while(i<=200):
+    i=i+5
+    print(i)'''
+
+
+##print only multiples of 8 from <user input> to <user input> while
+n=int(input("enter number:"))
+i=0
+while(i<n):
+    i=i+1
+    m=8*i
+    print(m)
+
