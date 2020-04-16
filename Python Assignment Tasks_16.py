21247a577a33347cc9ac5aeffe4bd670b3a98989
Sulekha-code <61930656+Sulekha-code@users.noreply.github.com>
2020-04-16 18:30:03 +0530

    Assignment_16
    For if else questions

---
+++ Python Assignment Tasks_16.py
@@ -0,0 +1,59 @@
+'''#user 10  user 30
+# multiples of 8 list
+#clues for [] append
+
+a=int(input("value 1:"))
+b=int(input("value 2:"))
+c=[]
+for i in range(a,b):
+      if i%8==0:
+          c.append(i)
+          
+print(c)
+
+#user 10  user 30
+# multiples of 8 list
+# multiples 0f 9 list
+a=int(input("value 1:"))
+b=int(input("value 2:"))
+e=int(input("value 3:"))
+f=int(input("value 4:"))
+c=[]
+d=[]
+for i in range(a,b):
+    if i%8==0:
+        c.append(i)
+print(c)
+for i in range(e,f):
+    if i%9==0:
+        d.append(i)
+print(d)
+
+
+#user 10  user 30
+# odd number
+# even numbers
+#clues for [] append two list
+a=int(input("value 1:"))
+b=int(input("value 2:"))
+c=[]
+d=[]
+e=int(input("value 3:"))
+f=int(input("value 4:"))
+for i in range(a,b):
+    if(i%2==0):
+        c.append(i)
+print(c)
+for i in range(e,f):
+    if(i%2!=0):
+        d.append(i)
+print(d)'''
+
+
+
+        
+'''a=["hello", "bcd", "gcd", "hhmmm", "python"]
+for c in a:
+   if c[0]=="h":
+       print(c)'''
+        
